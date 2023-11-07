f2fd2f127628f797886215c11ef4e5be3df425f3
Nathan <natedog7120@gmail.com>
2023-11-06 20:41:57 -0600

    readme

--- README.md
+++ README.md
@@ -7,7 +7,7 @@ WPF Design Template for me to refrence and grow on building windows applications
 I have started this project is a playground to expand my WPF Design and C# programming skills
 
 I plan to work on these items
-    - async
-    - Task
-    - xaml design
-    - stuff... :)
+    * async
+    * Task
+    * xaml design
+    * stuff... :)
--- WpfDesign/Wpfmvvm_02/Models/LoadCSV/LoadCSVItem.cs
+++ WpfDesign/Wpfmvvm_02/Models/LoadCSV/LoadCSVItem.cs
@@ -6,7 +6,29 @@ using System.Threading.Tasks;
 
 namespace Wpfmvvm_02.Models.LoadCSV
 {
-    internal class LoadCSVItem
+    public class LoadCSVItem
     {
+        public int ItemId { get; set; }
+        public string ItemName { get; set; }
+        public string Category { get; set; }
+        public string Supplier { get; set; }
+        public string Purchase { get; set; }
+
+        public int Quanity { get; set; }
+        public decimal UnitPrice { get; set; }
+   
+        public decimal ToalCost { get; set; }
+
+        public string Location { get; set; }
+        public int LocationLevel { get; set; }
+
+        public int Priority { get; set; }
+
+        public int DaysTillDelivery { get; set;}
+
+
+
+
+
     }
 }
