03151a8161791740f1eb4964e82a642dc63f2fdb
Nathan <86316586+Natey71@users.noreply.github.com>
2024-05-16 19:21:31 -0500

    Update openwindowcommand.cs

--- openwindowcommand.cs
+++ openwindowcommand.cs
@@ -9,3 +9,32 @@
         }
 
     }
+// how to use it
+ public class MainWindowVM
+ {
+
+     public ICommand CommandAddCustomer { get; private set; }
+     
+
+     public MainWindowVM()
+     {
+         CommandAddCustomer = new RelayCommand(OpenWindowAddCustomer);
+     }
+
+     private AddCustomerWindow _addCustomerWindow;
+     private void OpenWindowAddCustomer()
+     {
+         if (!Helper.Helper.IsWindowOpen<AddCustomerWindow>())
+         {
+             _addCustomerWindow = new AddCustomerWindow();
+             _addCustomerWindow.Show();
+         }
+         else
+         {
+             _addCustomerWindow.Topmost = true;
+             _addCustomerWindow.Focus();
+             _addCustomerWindow.Topmost = false; 
+         }
+
+     }
+ }
