bc28f2c8f4e31c3729f5ecc0b156bd5705aa7478
Nathan <86316586+Natey71@users.noreply.github.com>
2024-05-16 19:20:38 -0500

    Create openwindowcommand.cs
    a command used to check if window is open

---
+++ openwindowcommand.cs
@@ -0,0 +1,11 @@
+    public class Helper
+    {
+
+        public static bool IsWindowOpen<T>(string name = "") where T : Window
+        {
+            return string.IsNullOrEmpty(name)
+                ? Application.Current.Windows.OfType<T>().Any()
+                : Application.Current.Windows.OfType<T>().Any(x => x.Name.Equals(name));
+        }
+
+    }
